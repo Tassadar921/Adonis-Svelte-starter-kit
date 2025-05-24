bc429b433511b3dca9accfbe7f47ee462d2739c1
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-05-24 15:44:59 +0200

    Update init-logs-db.sh

--- init-logs-db.sh
+++ init-logs-db.sh
@@ -15,22 +15,22 @@ fi
 # Container name should match docker-compose service name
 DB_CONTAINER_NAME="${DB_HOST}"
 
-echo "Checking if database '$LOG_DB_DATABASE' exists in container '$DB_CONTAINER_NAME'..."
+echo "Checking if database '$LOGS_DB_DATABASE' exists in container '$DB_CONTAINER_NAME'..."
 
 # Check if database exists
-DB_EXISTS=$(docker exec -e PGPASSWORD="$LOG_DB_PASSWORD" -u postgres "$DB_CONTAINER_NAME" \
-  psql -U "$LOG_DB_USER" -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname = '$LOG_DB_DATABASE';")
+DB_EXISTS=$(docker exec -e PGPASSWORD="$LOGS_DB_PASSWORD" -u postgres "$DB_CONTAINER_NAME" \
+  psql -U "$LOGS_DB_USER" -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname = '$LOGS_DB_DATABASE';")
 
 if [ "$DB_EXISTS" = "1" ]; then
-  echo "Database '$LOG_DB_DATABASE' already exists. Skipping creation."
+  echo "Database '$LOGS_DB_DATABASE' already exists. Skipping creation."
 else
-  echo "Creating database '$LOG_DB_DATABASE'..."
-  docker exec -e PGPASSWORD="$LOG_DB_PASSWORD" -u postgres "$DB_CONTAINER_NAME" \
-    psql -U "$LOG_DB_USER" -d postgres -c "CREATE DATABASE \"$LOG_DB_DATABASE\";"
+  echo "Creating database '$LOGS_DB_DATABASE'..."
+  docker exec -e PGPASSWORD="$LOGS_DB_PASSWORD" -u postgres "$DB_CONTAINER_NAME" \
+    psql -U "$LOGS_DB_USER" -d postgres -c "CREATE DATABASE \"$LOGS_DB_DATABASE\";"
 
-  echo "Granting all privileges on '$LOG_DB_DATABASE' to user '$LOG_DB_USER'..."
-  docker exec -e PGPASSWORD="$LOG_DB_PASSWORD" -u postgres "$DB_CONTAINER_NAME" \
-    psql -U "$LOG_DB_USER" -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$LOG_DB_DATABASE\" TO \"$LOG_DB_USER\";"
+  echo "Granting all privileges on '$LOGS_DB_DATABASE' to user '$LOGS_DB_USER'..."
+  docker exec -e PGPASSWORD="$LOGS_DB_PASSWORD" -u postgres "$DB_CONTAINER_NAME" \
+    psql -U "$LOGS_DB_USER" -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$LOGS_DB_DATABASE\" TO \"$LOGS_DB_USER\";"
 
   echo "Database created and permissions granted."
 fi
