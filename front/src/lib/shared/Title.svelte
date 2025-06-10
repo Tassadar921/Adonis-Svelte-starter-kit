a953580f46e8a79e2416bc61d76f9f2c1fb8733e
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-06-10 13:56:39 +0200

    Update Title.svelte

--- front/src/lib/shared/Title.svelte
+++ front/src/lib/shared/Title.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+
     export let title: string;
     export let hasBackground: boolean = false;
 
-    $: document.title = title;
+    onMount((): void => {
+        document.title = title;
+    });
 </script>
 
 <h1 class="relative z-10 text-3xl {hasBackground ? 'text-white' : ''} font-bold mb-2">{title}</h1>
