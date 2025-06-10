ede4fcfa2f7dde4f14457d79f8d43c3363f40f0b
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-06-10 02:12:11 +0200

    save

--- front/env.d.ts
+++ front/env.d.ts
@@ -1,8 +1,8 @@
 interface ImportMetaEnv {
-    readonly VITE_PORT: number;
-    readonly VITE_FRONT_URI: string;
-    readonly VITE_API_BASE_URI: string;
-    readonly VITE_GITHUB_REPOSITORY: string;
+    readonly VITE_PORT: number; // injected by Docker
+    readonly VITE_FRONT_URI: string; // injected by Docker
+    readonly VITE_API_BASE_URI: string; // injected by Docker
+    readonly VITE_GITHUB_REPOSITORY: string; // injected by Docker
     readonly VITE_DEFAULT_IMAGE: string;
 }
 
