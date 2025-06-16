30e7e2bde8518bf4c0384adcf1a4a800c4dbbc5c
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-06-17 01:56:15 +0200

    Feat Sveltekit v2

--- front/src/lib/shared/Subtitle.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    export let style: string = '';
-    export let className: string = 'text-xl font-bold relative';
-</script>
-
-<h2 {style} class={className}><slot /></h2>
--- front/svelte.config.js
+++ front/svelte.config.js
@@ -1,5 +1,17 @@
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+const config = {
     preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter(),
+        alias: {
+            '#components': './src/components',
+            '#services': './src/services',
+            '#stores': './src/stores',
+            '#pages': './src/pages',
+        },
+    },
 };
+
+export default config;
