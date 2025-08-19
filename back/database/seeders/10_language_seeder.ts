041a6fcb072d0ebfe4df0d5ade831ba0c42d1340
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-08-19 12:24:36 +0200

    Update 10_language_seeder.ts

--- back/database/seeders/10_language_seeder.ts
+++ back/database/seeders/10_language_seeder.ts
@@ -7,24 +7,29 @@ import app from '@adonisjs/core/services/app';
 import FileService from '#services/file_service';
 import File from '#models/file';
 import FileTypeEnum from '#types/enum/file_type_enum';
+import FileRepository from '#repositories/file_repository';
 
 export default class extends BaseSeeder {
     public async run(): Promise<void> {
-        const languageRepository: LanguageRepository = new LanguageRepository();
         const fileService: FileService = new FileService();
+        const languageRepository: LanguageRepository = new LanguageRepository(fileService);
+        const fileRepository: FileRepository = new FileRepository();
 
         for (const language of [Language.LANGUAGE_ENGLISH, Language.LANGUAGE_FRENCH]) {
-            const path: string = await this.moveLanguageFlag(language.code);
-            const { size, mimeType, extension, name } = await fileService.getFileInfo(app.makePath(`${path}/${language.code}.svg`));
-            const flag: File = await File.create({
-                name,
-                path: `${path}/${language.code}.svg`,
-                extension,
-                mimeType,
-                size,
-                type: FileTypeEnum.LANGUAGE_FLAG,
-            });
-            await flag.refresh();
+            let flag: File | null = await fileRepository.findOneBy({ name: `${language.code}.svg` });
+            if (!flag) {
+                const path: string = await this.moveLanguageFlag(language.code);
+                const { size, mimeType, extension, name } = await fileService.getFileInfo(app.makePath(`${path}/${language.code}.svg`));
+                flag = await File.create({
+                    name,
+                    path: `${path}/${language.code}.svg`,
+                    extension,
+                    mimeType,
+                    size,
+                    type: FileTypeEnum.LANGUAGE_FLAG,
+                });
+                await flag.refresh();
+            }
 
             if (!(await languageRepository.findOneBy({ code: language.code }))) {
                 await Language.create({
