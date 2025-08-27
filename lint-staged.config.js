9935a2ecff8e385d5d69b649f6975a03b2e087b5
Paul Cuisinier <paul.lecuisinier@gmail.com>
2025-08-27 16:20:00 +0200

    test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,17 @@
 import patterns from "./format/patterns.js";
 import micromatch from "micromatch";
+import { relative } from "path";
+import { cwd } from "process";
 
 export default {
     "*": (filenames) => {
         console.log('ici : ', filenames)
         const commands = [];
 
+        const relativeFiles = filenames.map(f => relative(cwd(), f));
+
         Object.keys(patterns).forEach((folder) => {
-            const matchedFiles = micromatch(filenames, `${folder}/${patterns[folder]}`);
+            const matchedFiles = micromatch(relativeFiles, `${folder}/${patterns[folder]}`);
             console.log('là : ', `${folder}/${patterns[folder]}`, matchedFiles)
             if (matchedFiles.length) {
                 commands.push(`npx prettier --write ${matchedFiles.join(" ")}`);
